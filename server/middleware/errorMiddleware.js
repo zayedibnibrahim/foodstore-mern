21a47cedbe90fb99df32be010bf3759c1d4013f3
Shakiba448 <shakiba448@gmail.com>
2021-07-07 21:59:30 +0600

    React native

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -9,7 +9,6 @@ const notFound = (req, res, next) => {
 };
 
 const errorHandler = (err, req, res, next) => {
-  console.log(req);
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
 
